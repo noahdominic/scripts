576806aa0baebb4c528cdb9106a8b9cb15824eb1
Noah Dominic <noah@noahdominic.com>
2024-01-27 18:37:22 +0800

    upload: My current Windows Powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,64 @@
+# ============================================================================
+# Functions
+# ============================================================================
+
+## mkcd - Make a new dir $name and cd into $name
+##
+function Make-NewDirectory {
+  param(
+    [string]$DirectoryName
+  )
+  if (-not (Test-Path $DirectoryName)) {
+    New-Item -ItemType Directory -Path $DirectoryName | Out-Null
+    Set-Location -Path $DirectoryName
+  } else {
+    Write-Host "Directory '$DirectoryName' already exists."
+  }
+}
+
+New-Alias -Name mkcd -Value Make-NewDirectory
+
+## mkcd - Make a new dir $name and cd into $name
+##
+function Make-File {
+  param(
+    [string]$FileName
+  )
+  if (-not (Test-Path $FileName)) {
+    New-Item -ItemType File -Path $FileName 
+  } else {
+    Write-Host "File '$FileName' already exists."
+  }
+}
+
+New-Alias -Name mkfile -Value Make-File
+
+function sudo {
+    Start-Process @args -verb runas
+}
+
+# ============================================================================
+# Development - Proj Management
+# ============================================================================
+
+## Git
+New-Alias -Name ga -Value 'git add'
+New-Alias -Name gadd -Value 'git add'
+New-Alias -Name gcommit -Value 'git commit'
+function gcm {
+    param([string]$message)
+    & git commit -m $message
+}
+New-Alias -Name gcam -Value 'git commit -am'
+New-Alias -Name gpush -Value 'git push'
+New-Alias -Name gstat -Value 'git status'
+function ghash {
+    param([string]$object)
+    & git hash-object -w $object
+}
+New-Alias -Name gundo -Value 'git reset HEAD~'
+New-Alias -Name gb -Value 'git branch'
+New-Alias -Name gco -Value 'git checkout'
+New-Alias -Name gcheckout -Value 'git checkout'
+New-Alias -Name grebase -Value 'git rebase'
+New-Alias -Name gmerge -Value 'git merge'
